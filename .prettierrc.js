1994f13dc91cdfa561c510650417236a8b9ed438
BenedictLang <benedict.lang@outlook.de>
2024-07-07 04:49:48 +0200

    feat(config): Add tailwind and setup lint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   singleQuote: true,
   printWidth: 120,
+  tabWidth: 2,
+  useTabs: true,
   overrides: [
     {
       files: '*.scss',
       options: {
-        singleQuote: false,
+        singleQuote: true,
       },
     },
   ],
