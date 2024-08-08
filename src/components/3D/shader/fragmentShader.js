5ca8b8d6596e51ee6f72b1ef3c1a612160f62eac
BenedictLang <benedict.lang@outlook.de>
2024-08-09 01:12:27 +0200

    feat(): add pulsating brightness to fragment

--- src/components/3D/shader/fragmentShader.js
+++ src/components/3D/shader/fragmentShader.js
@@ -1,12 +1,17 @@
 const fragmentShader = `
+uniform float u_time;
+uniform float u_frequency;
+uniform float u_intensity;
 uniform float u_red;
 uniform float u_green;
 uniform float u_blue;
-uniform float u_intensity;
+
+varying vec3 v_position;
 
 void main() {
-    vec3 color = vec3(u_red, u_green, u_blue) * u_intensity;
-
+    float brightness = sin(u_frequency * length(v_position) - u_time) * 0.2 + 0.9;
+    vec3 color = vec3(u_red, u_green, u_blue) * brightness * u_intensity;
+    
     float dist = length(gl_PointCoord - vec2(0.5));
     if (dist > 0.5) discard;
 
