8a3aba167c7324c950ccb664d9c22a4e2d72831d
BenedictLang <benedict.lang@outlook.de>
2024-08-15 20:57:12 +0200

    fix(Next): disable image optimization

--- next.config.js
+++ next.config.js
@@ -33,6 +33,10 @@ const nextConfig = {
 		WORDPRESS_MENU_LOCATION_NAVIGATION: process.env.WORDPRESS_MENU_LOCATION_NAVIGATION || 'PRIMARY',
 		WORDPRESS_PLUGIN_SEO: parseEnvValue(process.env.WORDPRESS_PLUGIN_SEO, 'false'),
 	},
+	images: {
+		// The image optimization needs to be disabled in a static export
+		unoptimized: true,
+	},
 	output: 'export',
 };
 
