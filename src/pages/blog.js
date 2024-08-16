38bad100fbb34b1a510c1bf3dfedb0a2ee4d5919
BenedictLang <benedict.lang@outlook.de>
2024-08-16 05:38:17 +0200

    feat(Blog): add blog site

---
+++ src/pages/blog.js
@@ -0,0 +1,58 @@
+import useSite from 'hooks/use-site';
+import { getPaginatedPosts } from 'lib/posts';
+import { WebsiteJsonLd } from 'lib/json-ld';
+import styles from 'styles/pages/Home.module.scss';
+
+import Layout from 'components/Layouts/Layout';
+import Section from 'components/Sections/Section';
+import Container from 'components/ContainerElements/Container';
+import PostCard from 'components/PostCard';
+import Pagination from 'components/Pagination';
+
+export default function Home({ posts, pagination }) {
+	const { metadata = {} } = useSite();
+	const { title } = metadata;
+
+	return (
+		<Layout>
+			<WebsiteJsonLd siteTitle={title} />
+			<Section>
+				<Container>
+					<h2 className="sr-only">Posts</h2>
+					<ul className={styles.posts}>
+						{posts.map((post) => {
+							return (
+								<li key={post.slug}>
+									<PostCard post={post} />
+								</li>
+							);
+						})}
+					</ul>
+					{pagination && (
+						<Pagination
+							addCanonical={false}
+							currentPage={pagination?.currentPage}
+							pagesCount={pagination?.pagesCount}
+							basePath={pagination?.basePath}
+						/>
+					)}
+				</Container>
+			</Section>
+		</Layout>
+	);
+}
+
+export async function getStaticProps() {
+	const { posts, pagination } = await getPaginatedPosts({
+		queryIncludes: 'archive',
+	});
+	return {
+		props: {
+			posts,
+			pagination: {
+				...pagination,
+				basePath: '/posts',
+			},
+		},
+	};
+}
